32bca36e7ace214a0a0d0bd428a646f19334d33c
Julie <juliayakutik@gmail.com>
2020-06-22 00:20:15 +0300

    Step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,4 +4,6 @@ import program from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows the difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
